d044130d0cdf10906c5f17302ad486af8f2e8485
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2022-07-09 16:21:27 -0300

    Criando cabeçalho da aplicação

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import styles from './Header.module.css';
+
+import logoCodelandia from '../assets/logo.svg';
+
+const githubUrl = 'https://github.com/LeticiaBarbosa-dev';
+const linkedinUrl = 'https://www.linkedin.com/in/leticia-pbs/';
+
+export function Header() {
+    return (
+        <header className={styles.header}>
+            <img src={logoCodelandia} alt="Logotipo do condelândia" />
+            <div>
+                <a target='_blank' className={styles.buttonGithub} href={githubUrl}>Meu Github</a>
+                <a target='_blank' className={styles.buttonLinkedin} href={linkedinUrl}>Meu Linkedin</a>
+            </div>
+        </header>
+    );
+}
