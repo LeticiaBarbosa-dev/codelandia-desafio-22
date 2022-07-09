0cf5bb5c0ead5df6e998e1fc89721777d939541b
LeticiaBarbosa-dev <leticiapereiraab@gmail.com>
2022-07-09 18:38:32 -0300

    Criando banner principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import { Header } from "./components/Header";
-import './styles/global.css';
+import "./styles/global.css";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Header />
+      <div className="bannerPrincipal">
+        <span>comunidade dev</span>
+        <h1>
+          Projetos da <br />
+          comunidade Codelândia
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec felis<br />
+          ligula, accumsan nec cursus in, eleifend sit amet dui.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
